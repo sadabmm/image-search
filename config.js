0ec67882acf1bd4940c12035ea7b390444c0e479
Sadab M. Mahmud <30743415+sadabmm@users.noreply.github.com>
2018-02-18 16:20:48 +0600

    Removed my private keys

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  API_KEY: 'AIzaSyCYksP7jA6fPPHSrlaQbzrNV05z0iSTLks',
-  SEARCH_ENGINE_ID: '001431289734333889715:nihfdqib3mo',
-  MONGO_URL: 'mongodb://sadabmm:4727792@ds125318.mlab.com:25318/smm'
+  //Put your api key, search engine id and mongo url here
+  API_KEY: '',
+  SEARCH_ENGINE_ID: '',
+  MONGO_URL: ''
 };
